9d01ac79c5a2e34a8c7168bf32e553ec9e139162
TejasBhitle <bhitletejas@gmail.com>
2017-08-06 00:14:25 +0530

    constructor replaced by data model

--- app/src/main/java/com/codeblooded/gamedb/model/Game.kt
+++ app/src/main/java/com/codeblooded/gamedb/model/Game.kt
@@ -3,16 +3,10 @@ package com.codeblooded.gamedb.model
 /**
  * Created by tejas on 8/4/17.
  */
-class Game{
-
-    var id:Int = 0
-    var name: String? = null
-    var url: String? = null
-    var summary: String? = null
-    var storyline: String? = null
-    var popularity: Double = 0.0
-    var rating: Double = 0.0
-
-
-
-}
+data class Game(var id: Int,
+                var name: String? = null,
+                var summary : String? = null,
+                var storyline: String? = null,
+                var popularity: Double,
+                var rating: Double
+                )
