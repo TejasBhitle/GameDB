9ede3b440084e7e3556fb553520d8d98f1376dd4
Samriddha Basu <samriddhabasu@gmail.com>
2017-08-27 23:44:50 +0530

    Remove unnecessary API call and IdUtils class

--- app/src/main/java/com/codeblooded/gamedb/ui/fragments/GameListFragment.kt
+++ app/src/main/java/com/codeblooded/gamedb/ui/fragments/GameListFragment.kt
@@ -6,19 +6,18 @@ import android.content.SharedPreferences
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
-import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import android.widget.Toast
-import com.codeblooded.gamedb.*
+import com.codeblooded.gamedb.POPULARITY
+import com.codeblooded.gamedb.PREFERENCES
+import com.codeblooded.gamedb.R
+import com.codeblooded.gamedb.SORT
 import com.codeblooded.gamedb.adapters.GameListAdapter
 import com.codeblooded.gamedb.model.Game
-import com.codeblooded.gamedb.util.IdsUtil
 import com.codeblooded.gamedb.util.RestClient
-import com.loopj.android.http.AsyncHttpResponseHandler
 import com.loopj.android.http.JsonHttpResponseHandler
 import com.loopj.android.http.RequestParams
 import cz.msebera.android.httpclient.Header
@@ -64,35 +63,33 @@ class GameListFragment : Fragment() {
         progressDialog = ProgressDialog(context)
         progressDialog.setMessage("Fetching Games")
         progressDialog.show()
-        var idsUtil: IdsUtil = IdsUtil("/games/?order=" + pref.getString(SORT, POPULARITY), object : IdCallBackListener {
-            override fun method(ids: String) {
 
-                RestClient.get("/games/$ids", RequestParams(), object : JsonHttpResponseHandler() {
+        RestClient.addHeaders()
+        RestClient.get("/games/?fields=*&order=" + pref.getString(SORT, POPULARITY), RequestParams(), object : JsonHttpResponseHandler() {
 
-                    override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject?) {
-                        super.onSuccess(statusCode, headers, response)
-                    }
-
-                    override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONArray?) {
-                        super.onSuccess(statusCode, headers, response)
-                        updateUI(response)
-                    }
-
-                    override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONObject?) {
-                        super.onFailure(statusCode, headers, throwable, errorResponse)
-                    }
-
-                    override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONArray?) {
-                        super.onFailure(statusCode, headers, throwable, errorResponse)
-                    }
-
-                    override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
-                        super.onFailure(statusCode, headers, responseString, throwable)
-                    }
-
-                })
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONObject?) {
+                super.onSuccess(statusCode, headers, response)
             }
+
+            override fun onSuccess(statusCode: Int, headers: Array<out Header>?, response: JSONArray?) {
+                super.onSuccess(statusCode, headers, response)
+                updateUI(response)
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONObject?) {
+                super.onFailure(statusCode, headers, throwable, errorResponse)
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, throwable: Throwable?, errorResponse: JSONArray?) {
+                super.onFailure(statusCode, headers, throwable, errorResponse)
+            }
+
+            override fun onFailure(statusCode: Int, headers: Array<out Header>?, responseString: String?, throwable: Throwable?) {
+                super.onFailure(statusCode, headers, responseString, throwable)
+            }
+
         })
+
     }
 
     fun updateUI(response: JSONArray?) {
@@ -116,7 +113,7 @@ class GameListFragment : Fragment() {
         }
         progressDialog.cancel()
         if (games.size == 0)
-            textview.text = "No Games"
+            textview.text = getString(R.string.empty_list)
 
         recyclerView.adapter = GameListAdapter(context, games)
 
